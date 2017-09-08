e388a56766442ab296db4392205301e38fe1fd17
Carlynn <chick04sd@gmail.com>
2017-09-07 18:30:20 -0700

    Review GET, buttons, and containers up to date

--- public/scripts/reviews.js
+++ public/scripts/reviews.js
@@ -8,7 +8,7 @@ $.ajax({
   error: handleError
 });
 
-var renderReviews = console.log("Reviews Rendered!")
+// var renderReviews = console.log("Reviews Rendered!")
 
 function handleError(err){
   console.log('There has been an error: ', err);
@@ -16,21 +16,28 @@ function handleError(err){
 //Step 1a, 2 of 3:
 //This runs through the forEach loop. Each item in the api will be shown and the renderVenue will display this per the function below
 function renderReviews(reviews) {
+  console.log(reviews);
   reviews.forEach(function(review) {
+    console.log('This is a review: ', review);
     renderReview(review);
   });
 }
 
+
+
 //Step 1a, part 3 of 3:
 function renderReview(review) {
-  var reviewHtml = (`<div class="row" data-review-id="${review._id}" style="margin-bottom: 15px;">
-  <div class="col-sm-6">
-  <h4><b>${review.reviewerName}</b></h4>
+  var reviewHtml = (`
+<div class="review-card">
+    <div class="row" data-review-id="${review._id}" style="margin-bottom: 15px;">
+  <div class="col-sm-12">
+    <div class="col-sm-6" style="text-align: left; padding-left: 0">
+      <h4><b>${review.reviewerName}</b></h4>
+    </div>
+    <div class="col-sm-6" style="text-align: right; padding-left: 0">      <h4><b>${review.reviewerRating} Stars</b></h4></div>
+    </div>
   </div>
-  <div class="col-sm-6" style="text-align: right">
-  <span><h4><b>${review.reviewerRating} Stars</b></h4></span>
-  </div>
-  </div>
+
   <div class="row">
   <div class="col-sm-12">
   <h4><b>Review:</b></h4>
@@ -39,6 +46,14 @@ function renderReview(review) {
   <div class="row">
   <div class="col-sm-12">
   <p>${review.reviewerNotes}</p>
+  </div>
+  <div class="col-sm-12">
+  <span>
+  <button class='btn btn-info tgl-btn editReview' data-view-id="inserthere" style="width: 125px; margin-right: 10px;" data-toggle="modal" data-target="#edit-review-modal">Edit Review</button>
+  <button class='btn btn-danger deleteReview' style="width: 125px; margin-right: 10px;">Delete</button>
+  </span>
+  </div>
+  </div>
   </div>
   </div>
   </div>
