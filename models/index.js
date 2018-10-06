f64273bc7f6866fffcc867971a1d2ad3dcd01107
LAPTOP-NHRLSVER\malup <malu.peralta808@gmail.com>
2018-10-05 23:52:59 -0700

    updated connection string

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api", {useMongoClient: true});
+mongoose.connect( process.env.MONGODB_URI || "mongodb://MaluPalu:pubhub1234!@ec2-18-206-214-214.compute-1.amazonaws.com:27017/personal-api", {useMongoClient: true});
 mongoose.Promise = global.Promise;  // use native Promise
 
 module.exports.PubHub = require("./pubHubs.js");
